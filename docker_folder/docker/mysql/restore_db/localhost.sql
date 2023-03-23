ec946c4c2798c3e1ca9b78d3b0f2e659ec289cb5
Wallice <wallicecarias@gmail.com>
2023-03-23 17:35:24 +0100

    salvando restore db

---
+++ docker_folder/docker/mysql/restore_db/localhost.sql
@@ -0,0 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 23-Mar-2023 às 13:20
+-- Versão do servidor: 5.7.23-23
+-- versão do PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `corrie62_corriela`
+--
+CREATE DATABASE IF NOT EXISTS `db_teste` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `db_teste`;
