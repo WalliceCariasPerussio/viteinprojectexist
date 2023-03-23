67445b5efc0ea988d4947367303f3792edd0a870
Wallice <wallicecarias@gmail.com>
2023-03-23 02:49:58 +0100

    melhorando o ping

--- index.php
+++ index.php
@@ -11,19 +11,19 @@
     <?php
       function vueRender(){
         
+       
+        $manifest = file_get_contents(__dir__. '/vue/dist/manifest.json');
+        $manifest = json_decode($manifest, true);
+        $css 	= $manifest['vue/src/main.css']['file'];
+        $js 	= $manifest['vue/src/main.js']['file'];
+        // corrigir a producao. O caminho deve ser relativo
+        $html = "<link rel='stylesheet' href='vue/dist/$css' />";
+        $html .= "<script type='module' src='vue/dist/$js'></script>";
+       
         if(($_SERVER['SERVER_NAME'] === 'docker.localhost')){
-          $html =  "<script type='module' src='http://localhost:5173/@vite/client'></script>";
+          $html .=  "<script type='module' src='http://localhost:5173/@vite/client'></script>";
           $html .=  "<script type='module' src='http://localhost:5173/src/main.js'></script>";
-        }else{
-          $manifest = file_get_contents(__dir__. '/vue/dist/manifest.json');
-          $manifest = json_decode($manifest, true);
-          $css 	= $manifest['vue/src/main.css']['file'];
-          $js 	= $manifest['vue/src/main.js']['file'];
-          // corrigir a producao. O caminho deve ser relativo
-          $html = "<link rel='stylesheet' href='vue/dist/$css' />";
-          $html .= "<script type='module' src='vue/dist/$js'></script>";
         }
-
         return $html;
       }
 
